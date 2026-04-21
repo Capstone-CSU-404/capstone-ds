d0b920fd7d520f93e780e8e3f416ffaf72c8b00a
Firman Adi Kurniawan <firmanadik09@gmail.com>
2026-04-21 19:32:41 +0700

    update akun

--- scraping/linkedinfix.py
+++ scraping/linkedinfix.py
@@ -17,8 +17,8 @@ import os
 # ─────────────────────────────────────────────
 # KONFIGURASI
 # ─────────────────────────────────────────────
-LINKEDIN_EMAIL    = "firmanadik09@gmail.com"   # ← ganti
-LINKEDIN_PASSWORD = "Fak_1809"          # ← ganti
+LINKEDIN_EMAIL    = "akunmu@gmail.com# ← ganti
+LINKEDIN_PASSWORD = "Passwordmu"   # ← ganti
 
 TARGET_JOB_ROLES = [
     "Web Developer",
