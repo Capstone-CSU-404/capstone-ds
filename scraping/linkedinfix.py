23eb1424dac5b4d6833ac54bb1bb3690432f5014
Firman Adi Kurniawan <firmanadik09@gmail.com>
2026-05-04 23:36:36 +0700

    update

--- scraping/linkedinfix.py
+++ scraping/linkedinfix.py
@@ -21,7 +21,7 @@ LINKEDIN_EMAIL    = "firmanadik09@gmail.com" # ← ganti
 LINKEDIN_PASSWORD = "Fak_1809"   # ← ganti
 
 TARGET_JOB_ROLES = [
- "Data Engineer", "IT", "Software Engineer"
+ "Developer", "Software","Progammer"
 ]
 
 LOCATION       = "Indonesia"
